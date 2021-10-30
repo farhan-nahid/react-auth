422e40d0ba6ae0fc404b8696a3ecf49cb6f089f4
gesora96 <nahidahmed9933@gmail.com>
2021-10-30 17:58:59 +0600

    Implement Auth Redirect

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Header from './components/Header/Header';
 import Home from './components/Home/Home';
 import Login from './components/Login/Login';
 import NotFound from './components/NotFound/NotFound';
+import PrivateRoute from './components/PrivateRoute/PrivateRoute';
 import Register from './components/Register/Register';
 import Shipping from './components/Shipping/Shipping';
 import AuthProvider from './context/AuthProvider';
@@ -19,7 +20,10 @@ function App() {
           <Route path='/home' component={Home} />
           <Route path='/login' component={Login} />
           <Route path='/register' component={Register} />
-          <Route path='/shipping' component={Shipping} />
+          <PrivateRoute path='/shipping'>
+            <Shipping />
+          </PrivateRoute>
+          {/* <PrivateRoute path='/shipping' component={Shipping} /> */}
           <Route path='*' component={NotFound} />
         </Switch>
       </Router>
